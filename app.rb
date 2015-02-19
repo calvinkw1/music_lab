55d82ad5c0d5472a6737d8a76098e6ada05692e5
Calvin Wang <calvinkw1@gmail.com>
2015-02-19 09:07:58 -0800

    finished hw

--- app.rb
+++ app.rb
@@ -1,16 +1,58 @@
 require 'sinatra'
 require 'sinatra/reloader'
 require 'pry'
+require 'sinatra/activerecord'
+require './models/artists'
 require 'better_errors'
 configure :development do
   use BetterErrors::Middleware
   BetterErrors.application_root = __dir__
 end
 
+# ============= GET ============= #
+
 get '/' do
-  redirect '/index'
+  redirect '/artists'
 end
 
-get '/index' do
+get '/artists' do
+  @artists = Artist.all
   erb :index
+end
+
+get '/artists/new' do
+  erb :new_artist
+end
+
+get '/artists/:id' do
+  @artist = Artist.find params[:id]
+  erb :show_artist
+end
+
+get '/artists/:id/edit' do
+  @artist = Artist.find params[:id]
+  erb :edit_artist
+end
+
+# ============= POST ============= #
+
+post '/artists' do
+  @artist = Artist.create params
+  redirect '/artists'
+end
+
+# ============= PUT ============= #
+
+put '/artists/:id' do
+  artist = Artist.find(params[:id])
+  artist.update name: params[:name]
+  redirect '/artists'
+end
+
+# ============= DELETE ============= #
+
+delete '/artists/:id' do
+  artist = Artist.find(params[:id])
+  artist.destroy
+  redirect '/artists'
 end
---
+++ db/seeds.rb
@@ -0,0 +1,6 @@
+Artist.destroy_all
+Artist.create({:name => "Ed Sheeran"})
+Artist.create({:name => "Taylor Swift"})
+Artist.create({:name => "Linkin Park"})
+Artist.create({:name => "Boyz II Men"})
+Artist.create({:name => "Swedish House Mafia"})
